0292c403d9a4ce0ad36e623888ea95d11edd596f
Dαnjs ◮ <142186063+danjs00@users.noreply.github.com>
2025-02-18 12:55:34 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+(function () {
+  "use strict";
+
+  // ======= Sticky
+  window.onscroll = function () {
+    const header = document.querySelector(".header");
+    const sticky = header.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
+
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+
+    // === logo change
+    logo.src = header.classList.contains("sticky")
+      ? "https://i.ibb.co/4n7wyy6z/LOGO-1.png"
+      : "https://i.ibb.co/G3P7HN4V/LOGO.png";
+
+    // Mostrar u ocultar el botón de volver arriba
+    const backToTop = document.querySelector(".back-to-top");
+    backToTop.style.display = window.scrollY > 50 ? "flex" : "none";
+  };
+
+// ==== Menú scroll
+document.querySelectorAll(".menu-scroll").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = elem.getAttribute("href").substring(1); // Eliminar '#' para obtener el id
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start", // Asegura que la sección inicie desde la parte superior
+      });
+    }
+  });
+});
+
+// === Resalta el menú activo
+function onScroll() {
+  const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+  document.querySelectorAll(".menu-scroll").forEach((currLink) => {
+    const targetId = currLink.getAttribute("href").substring(1); // Eliminar '#'
+    const refElement = document.getElementById(targetId);
+    if (refElement) {
+      const refTop = refElement.offsetTop;
+      const refBottom = refTop + refElement.offsetHeight;
+      if (scrollPos >= refTop - 550 && scrollPos < refBottom - 550) {
+        document.querySelector(".menu-scroll.active")?.classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  });
+}
+document.addEventListener("scroll", onScroll);
+
+  // === Cerrar menú al hacer clic en un enlace
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.querySelectorAll(".menu-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+    navbarCollapse.classList.toggle("show");
+  });
+
+  // === Submenús
+  document.querySelectorAll(".nav-item-has-children").forEach((elem) => {
+    elem.querySelector("a").addEventListener("click", () => {
+      elem.querySelector(".submenu").classList.toggle("show");
+    });
+  });
+
+  // === Lightbox para videos
+  const myGallery = GLightbox({
+    href: "assets/images/home/videoen.mp4",
+    type: "video",
+    source: "local",
+    width: 900,
+    autoplayVideos: true,
+  });
+  
+    // === Volver arriba con animación
+    function scrollToTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  
+    document.querySelector(".back-to-top").addEventListener("click", scrollToTop);
+})();
